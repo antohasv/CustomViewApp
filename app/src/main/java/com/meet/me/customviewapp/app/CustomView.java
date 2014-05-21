844622ef83a037ddb3b2e348d8efafddf3e0c787
toha <tohasvs@gmail.com>
2014-05-21 22:15:46 +0400

    add scroller

--- app/src/main/java/com/meet/me/customviewapp/app/CustomView.java
+++ app/src/main/java/com/meet/me/customviewapp/app/CustomView.java
@@ -2,17 +2,30 @@ package com.meet.me.customviewapp.app;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.os.Handler;
+import android.os.Message;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.ViewGroup;
+import android.widget.Scroller;
 
 public class CustomView extends ViewGroup {
 
-    private int mHeight;
-    private int mWidth;
+    public static final int MSG_SCROLL_RIGHT = 1000;
+    public static final int MSG_SCROLL_LEFT = 1001;
+
+    public static final int WIN_OFFSET = 10;
+    public static final int DIS_JUMP = 10;
+
+    private int mScreenHeight;
+    private int mScreenWidth;
+
+    private int mMenuWidth;
+    private Scroller mScroller;
+
+    private MyHandler mHandler;
 
     public CustomView(Context context) {
         this(context, null);
@@ -24,6 +37,9 @@ public class CustomView extends ViewGroup {
 
     public CustomView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        mScroller = new Scroller(getContext());
+        mHandler = new MyHandler(this);
+
         createViews();
     }
 
@@ -44,9 +60,11 @@ public class CustomView extends ViewGroup {
 
         measureChild(widthMeasureSpec, heightMeasureSpec);
 
-        mWidth = MeasureSpec.getSize(widthMeasureSpec);
-        mHeight = MeasureSpec.getSize(heightMeasureSpec);
-        setMeasuredDimension(mWidth, mHeight);
+        mScreenWidth = MeasureSpec.getSize(widthMeasureSpec);
+        mScreenHeight = MeasureSpec.getSize(heightMeasureSpec);
+        mMenuWidth = mScreenWidth / 2;
+
+        setMeasuredDimension(mScreenWidth, mScreenHeight);
     }
 
     private float oldX;
@@ -62,14 +80,18 @@ public class CustomView extends ViewGroup {
                 oldX = x;
                 oldY = y;
                 return false;
-            case MotionEvent.ACTION_MOVE:
-                Log.d("TAG", "MOVEEEE");
+            case MotionEvent.ACTION_MOVE: {
                 float deltaX = x - oldX;
                 float deltaY = y - oldY;
 
-                if (deltaX > 0 && deltaX >= ViewConfiguration.getTouchSlop() && (Math.abs(deltaX) - Math.abs(deltaY)) > 0) {
+                if (deltaX > 0 && deltaX >= ViewConfiguration.get(getContext()).getScaledTouchSlop() && (Math.abs(deltaX) - Math.abs(deltaY)) > 0) {
                     return true;
                 }
+            }
+            case MotionEvent.ACTION_UP:
+                int deltaX = Math.round(x - oldX);
+                mMenuWidth = mMenuWidth + deltaX;
+                return true;
         }
         return super.onInterceptTouchEvent(ev);
     }
@@ -79,12 +101,23 @@ public class CustomView extends ViewGroup {
         float x = event.getX();
         float y = event.getY();
 
+        int deltaX = Math.round(x - oldX);
+
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 return true;
             case MotionEvent.ACTION_MOVE:
-                int deltaX = Math.round(x - oldX);
-                getChildAt(1).layout(-mWidth / 2 + deltaX, 0, mWidth / 2, mHeight);
+                getChildAt(1).layout(-mScreenWidth, 0, mMenuWidth + deltaX, mScreenHeight);
+                return true;
+            case MotionEvent.ACTION_UP:
+
+                if (x > mScreenWidth / 2) {
+                    mScroller.startScroll((int) x, 0, mScreenWidth - (int) x, 0, 2000);
+                    mHandler.sendEmptyMessage(MSG_SCROLL_RIGHT);
+                } else {
+                    mScroller.startScroll((int) x, 0, (int) x, 0, 2000);
+                    mHandler.sendEmptyMessage(MSG_SCROLL_LEFT);
+                }
                 return true;
         }
         return super.onTouchEvent(event);
@@ -99,7 +132,41 @@ public class CustomView extends ViewGroup {
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        getChildAt(0).layout(0, 0, mWidth, mHeight);
-        getChildAt(1).layout(-mWidth / 2, 0, mWidth / 2, mHeight);
+        getChildAt(0).layout(0, 0, mScreenWidth, mScreenHeight);
+        getChildAt(1).layout(-mScreenWidth, 0, WIN_OFFSET, mScreenHeight);
+    }
+
+    public Scroller getScroller() {
+        return mScroller;
+    }
+
+    private static class MyHandler extends Handler {
+
+        private CustomView mCustomView;
+        private Scroller mScroller;
+
+        MyHandler(CustomView customView) {
+            mCustomView = customView;
+            mScroller = customView.getScroller();
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            if (mScroller.computeScrollOffset()) {
+                switch (msg.what) {
+                    case MSG_SCROLL_RIGHT:
+                        mCustomView.getChildAt(1).layout(-mCustomView.mScreenWidth, 0, mScroller.getCurrX(), mCustomView.mScreenHeight);
+                        sendEmptyMessage(MSG_SCROLL_RIGHT);
+                        break;
+                    case MSG_SCROLL_LEFT:
+                        mCustomView.getChildAt(1).layout(-mCustomView.mScreenWidth, 0, -mScroller.getCurrX(), mCustomView.mScreenHeight);
+                        sendEmptyMessage(MSG_SCROLL_LEFT);
+                        break;
+                }
+            }
+
+        }
     }
 }
